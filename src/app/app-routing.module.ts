f61adac19daf207693cd547f106e833d3bba7da7
Gennadii <oracl.vpn@mail.ru>
2022-07-31 18:56:17 +1100

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+
+  { path: '', loadChildren: () => import('./mod-main/mod-main.module').then(m => m.ModMainModule) },
+
+  { path: 'admin', loadChildren: () => import('./mod-admin/mod-admin.module').then(m => m.ModAdminModule) },
+
+  { path: 'user', loadChildren: () => import('./mod-user/mod-user.module').then(m => m.ModUserModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
